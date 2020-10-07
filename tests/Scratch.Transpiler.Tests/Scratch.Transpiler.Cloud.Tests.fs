0598c5c20bb8a6c795c4c191e9f48d19a68577b3
wiinuk <wii.nukegara@gmail.com>
2020-10-07 21:19:31 +0900

    [WIP]

---
+++ tests/Scratch.Transpiler.Tests/Scratch.Transpiler.Cloud.Tests.fs
@@ -0,0 +1,36 @@
+﻿module Scratch.Transpiler.Cloud.Tests
+open Xunit
+open Scratch
+open Scratch.Transpiler.Tests
+open Scratch.Evaluator
+
+let withIncrementCloud c =
+    let mutable map = Map.empty
+    let incrementCloud = Cloud.poly { new ICloud with
+        override _.Set(n, v) = map <- Map.add n v map
+        override _.Get n =
+            let v =
+                match Map.tryFind n map with
+                | ValueSome x -> SValue.toNumber x + 1. |> SNumber
+                | _ -> SValue.sZero
+            map <- Map.add n v map
+            v
+    }
+    EvaluateConfig.withCloud incrementCloud c
+
+[<ReflectedDefinition; Persistent>]
+let mutable ``☁ x`` = 10
+
+[<Fact>]
+let cloudVariableTest() =
+    <@
+        let output = defineList []
+        whenGreenFlag {
+            ``☁ x`` <- 0
+            do! repeatAsync 3 {
+                SList.push output ``☁ x``
+            }
+        }
+    @>
+    |> startAsStdSpriteWith id withIncrementCloud
+    =? ["0"; "1"; "2"]
