abe9bdc2b23f3f9e9e8884bda036210aedcb704c
wiinuk <wii.nukegara@gmail.com>
2020-10-28 09:08:05 +0900

    fix

--- src/Scratch.Primitives/VOption.fs
+++ src/Scratch.Primitives/VOption.fs
@@ -2,7 +2,8 @@
 
 let inline isSome x = match x with ValueSome _ -> true | _ -> false
 let inline isNone x = match x with ValueNone -> true | _ -> false
-let inline map f = function ValueSome x -> ValueSome(f x) | ValueNone -> ValueNone
-let inline defaultValue x = function ValueSome x -> x | ValueNone -> x
-let inline box x = match x with ValueSome x -> Some x | ValueNone -> None
-let inline unbox x = match x with Some x -> ValueSome x | None -> ValueNone
+let inline map f = function ValueSome x -> ValueSome(f x) | _ -> ValueNone
+let inline bind f = function ValueSome x -> f x | _ -> ValueNone
+let inline defaultValue x = function ValueSome x -> x | _ -> x
+let inline box x = match x with ValueSome x -> Some x | _ -> None
+let inline unbox x = match x with Some x -> ValueSome x | _ -> ValueNone
