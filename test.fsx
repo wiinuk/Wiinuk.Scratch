b3a10c91d5d8ba2a401b5af5beefdb80db7c681b
wiinuk <wii.nukegara@gmail.com>
2020-11-13 15:56:04 +0900

    🚧

--- test.fsx
+++ test.fsx
@@ -1,5 +1,18 @@
 ﻿#load "Shell.fsx"
+open System.IO
 
 start "dotnet tool restore"
 start "dotnet paket restore"
-start "dotnet test ./tests/Scratch.Transpiler.Tests/ --blame"
+try
+    start "dotnet test ./tests/Scratch.Transpiler.Tests/ --blame"
+finally
+    let parent = "./tests/Scratch.Transpiler.Tests/TestResults/"
+    if Directory.Exists parent then
+        for xmlPath in Directory.EnumerateFiles(parent, "*.xml", SearchOption.AllDirectories) do
+            printfn ""
+            printfn $"{xmlPath}"
+            printfn "======================="
+            for line in File.ReadLines xmlPath do
+                printfn $"{line}"
+            printfn "======================="
+            printfn ""
